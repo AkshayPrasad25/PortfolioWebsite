0c6e4ff871f865d2b19fab259ca6294b96a0a0ca
AkshayPrasad25 <akshayprasad510@gmail.com>
2023-09-02 16:01:17 +0530

    Portfolio Final

--- my-app/src/components/Form.js
+++ my-app/src/components/Form.js
@@ -1,19 +1,34 @@
 import "./Form.css"
-import React from 'react'
+import {React, useRef} from 'react'
+import emailjs from '@emailjs/browser';
 
 const Form = () => {
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_89qv02u', 'template_pqw57ij', form.current, '04oTAt_60ZAxj1Aft')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
   return (
     <div className="form">
-        <form>
+        <form ref={form} onSubmit={sendEmail}>
             <label>Your Name</label>
-            <input type="text"></input>
+            <input type="text" name="user_name"></input>
             <label>Email</label>
-            <input type="text"></input>
+            <input type="text" name="user_email"></input>
             <label>Subject</label>
-            <input type="text"></input>
+            <input type="text" name="subject"></input>
             <label>Message</label>
-            <textarea rows="6" placeholder="Type your message here" />
-            <a href="mailto:akshaywork510@gmail.com" className="btn">Submit</a>
+            <textarea rows="6" placeholder="Type your message here" name="message" />
+            <button className="btn">Submit</button>
         </form>
     </div>
   )
