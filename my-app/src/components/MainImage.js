b2920734c6c1529893a93ec7bb195f85510dc383
AkshayPrasad25 <akshayprasad510@gmail.com>
2023-09-03 13:02:32 +0530

    Portfolio Final

--- my-app/src/components/MainImage.js
+++ my-app/src/components/MainImage.js
@@ -13,7 +13,7 @@ const MainImage = () => {
             <h1>Hi, I'm Akshay</h1>
             <div>
                 <Link to="/projects" className="btn">Projects</Link>
-                <Link to="/contact" className="btn btn-light">Contact</Link>
+                <Link to="/contact" className="btn">Contact</Link>
             </div>
         </div>
     </div>
